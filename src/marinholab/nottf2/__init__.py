cf77dbf83f92ac7cf2de81f312017585d7bf87a0
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-11-02 15:14:35 +0000

    [__init__] Fixing import for elementary rotations.

--- src/marinholab/nottf2/__init__.py
+++ src/marinholab/nottf2/__init__.py
@@ -1,5 +1,5 @@
 from marinholab.nottf2._quaternion_operations import quaternion_norm, quaternion_multiply
-from marinholab.nottf2._rotation_operations import rotation_inverse, rotx, roty, rotz
+from marinholab.nottf2._rotation_operations import rotation_inverse, rx, ry, rz
 
 # https://setuptools-git-versioning.readthedocs.io/en/stable/runtime_version.html
 from importlib.metadata import version, PackageNotFoundError
